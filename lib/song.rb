63e21bf2f40237175ea84682b98290499dc8a3fe
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-02-09 03:10:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,9 @@ class Song
 
   def self.genre_count
     genre_count= {}
-      @@genres.each { |genre|
+      @@genres.each |genre|
         if genre_count[genre]
-            genre_count[genre]+= 1}
+            genre_count[genre]+= 1
         else #if the genre doesn't exist in our hash
             genre_count[genre]= 1
         end
