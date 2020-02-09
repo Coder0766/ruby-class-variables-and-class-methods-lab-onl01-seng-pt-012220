552ec6f4730bdc98da2f216cd055ab0b2fdc108b
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-02-09 02:36:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
 
   def self.genre_count
     genre_count= {}
-      @@genre.each { |genre| }
+      @@genres.each { |genre| }
         if genre_count[genre]
             genre_count[genre]+= 1
         else #if the genre doesn't exist in our hash
